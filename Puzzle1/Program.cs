29161a62ff314ff4187c2148d32a8e8887fa3a52
jesse <jesse@JP-DESKTOP>
2023-12-07 20:04:04 -0500

    Add project files.

---
+++ Puzzle1/Program.cs
@@ -0,0 +1,25 @@
+﻿const int puzzleVer = 1;
+int finalAnswer = 0;
+
+StreamReader sr = new StreamReader("input.txt");
+string line = sr.ReadLine();
+
+while (line != null)
+{
+    switch(puzzleVer)
+    {
+        case 1:
+            {
+                string numString = new string(line.Where(char.IsDigit).ToArray());
+                string finalAnswerString = numString[0].ToString() + numString[numString.Length - 1].ToString();
+                finalAnswer += Convert.ToInt32(finalAnswerString);
+                break;
+            }
+        default:
+            throw new Exception("Incorrect Puzzle Version");
+    }
+
+    line = sr.ReadLine();
+}
+
+Console.WriteLine(finalAnswer.ToString());
